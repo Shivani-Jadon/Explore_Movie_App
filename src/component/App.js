496b16a8e93deead81b3f208282bd8478bcc4977
Shivani Jadon <shivanijadon98@gmail.com>
2020-10-06 00:17:42 +0530

    add store

--- src/component/App.js
+++ src/component/App.js
@@ -3,24 +3,26 @@ import { data } from '../data';
 import Navbar from '../component/Navbar';
 import MovieCard from '../component/MovieCard';
 
-function App() {
-  return (
-    <div className="App">
-    	<Navbar />
+class App extends React.Component {
+  render () {
+	  return (
+		<div className="App">
+			<Navbar />
 
-      	<main>
-        	<div>
-				<button className='tab-btn'>Movie List</button>
-				<button className='tab-btn'>Favourites</button>
-        	</div>
-			<div className='list'>
-				{data.map(movie => (
-					<MovieCard movie={movie}/>
-				))}
-			</div>
-		</main>
-    </div>
-  );
+			<main>
+				<div>
+					<button className='tab-btn'>Movie List</button>
+					<button className='tab-btn'>Favourites</button>
+				</div>
+				<div className='list'>
+					{data.map( (movie, index) => (
+						<MovieCard movie={movie} key={`movie_${index}`} />
+					))}
+				</div>
+			</main>
+		</div>
+	);
+	}
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+
 import './index.css';
 import App from './component/App';
+import movies from './reducer';
 
+const store = createStore(movies);
+console.log("Store", store);
+console.log("State", store.getState() );
 
 ReactDOM.render(
   <React.StrictMode>
